677628515533d5ef264c006af2d1e7a2dac42c1f
abhishek17551 <ababhishek113@gmail.com>
2025-05-07 13:01:48 +0530

    DB connection

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import { PORT } from './config/env.js'
 import userRouter from './routes/user.routes.js'
 import authRouter from './routes/auth.routes.js'
 import subscriptionRouter from './routes/subscription.routes.js'
+import connectToDatabase from './database/mongodb.js'
 
 const app= express()
 
@@ -14,7 +15,7 @@ app.get('/',(req,res) => {
     res.send('Welcome to the Subscription tracker API' )
 })
 
-app.listen(PORT, () => {
+app.listen(PORT, async () => {
     console.log(`Server successfully running on port ${PORT}!`);
-    
+    await connectToDatabase()
 })
